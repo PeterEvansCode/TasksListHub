5d33901e69c7a75635fb114f738c30c0b118890b
Peter Evans <peterevans.code@gmail.com>
2024-09-05 12:57:26 +0100

    Database funtionality
    Implemented basic SQL database

--- app/src/main/java/com/example/googletasksassistant/MainActivity.kt
+++ app/src/main/java/com/example/googletasksassistant/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.googletasksassistant
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -11,14 +12,15 @@ import com.example.googletasksassistant.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity(), TaskItemClickListener
 {
     private lateinit var binding : ActivityMainBinding
-    private lateinit var taskViewModel: TaskViewModel
+    private val taskViewModel: TaskViewModel by viewModels {
+        TaskItemModelFactory((application as TodoApplication).repository)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        taskViewModel = ViewModelProvider(this).get(TaskViewModel::class.java)
         binding.newTaskButton.setOnClickListener{
             NewTaskSheet(null).show(supportFragmentManager, "newTaskTag")
         }
--- app/src/main/java/com/example/googletasksassistant/NewTaskSheet.kt
+++ app/src/main/java/com/example/googletasksassistant/NewTaskSheet.kt
@@ -30,8 +30,8 @@ class NewTaskSheet(var taskItem: TaskItem?) : BottomSheetDialogFragment()
             val editable = Editable.Factory.getInstance()
             binding.name.text = editable.newEditable(taskItem!!.name)
             binding.desc.text = editable.newEditable(taskItem!!.desc)
-            if(taskItem!!.dueTime != null){
-                dueTime = taskItem!!.dueTime!!
+            if(taskItem!!.dueTime() != null){
+                dueTime = taskItem!!.dueTime()!!
                 updateTimeButtonText()
             }
         }
@@ -74,13 +74,17 @@ class NewTaskSheet(var taskItem: TaskItem?) : BottomSheetDialogFragment()
     {
         val name = binding.name.text.toString()
         val desc = binding.desc.text.toString()
+        val dueTimeString = if(dueTime == null) null else TaskItem.timeFormatter.format(dueTime)
 
         if(taskItem == null){
-            val newTask = TaskItem(name, desc, dueTime, null)
+            val newTask = TaskItem(name, desc, dueTimeString, null)
             taskViewModel.addTaskItem(newTask)
         }
         else{
-            taskViewModel.updateTaskItem(taskItem!!.id, name, desc, dueTime)
+            taskItem!!.name = name
+            taskItem!!.desc = desc
+            taskItem!!.dueTimeString = dueTimeString
+            taskViewModel.updateTaskItem(taskItem!!)
         }
 
         binding.name.setText("")
