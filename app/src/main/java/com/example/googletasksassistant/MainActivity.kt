fa59aa3e3f55d575b2799083a8f9b0f83d8003e8
Peter Evans <peterevans.code@gmail.com>
2024-09-08 20:45:29 +0100

    Working drawer menu

--- app/src/main/java/com/example/googletasksassistant/MainActivity.kt
+++ app/src/main/java/com/example/googletasksassistant/MainActivity.kt
@@ -1,20 +1,78 @@
 package com.example.googletasksassistant
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.example.googletasksassistant.databinding.ActivityMainBinding
+import com.example.googletasksassistant.databinding.FragmentMainTaskListBinding
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Check if the Fragment is already added to avoid adding it multiple times
-        if (savedInstanceState == null) {
-            val fragment = MainTaskListFragment()
+        drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        // Register an OnBackPressedCallback to handle the back press
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                } else {
+                    // If the drawer isn't open, proceed with the normal back press
+                    isEnabled = false  // Disable this callback so the system can handle the back press
+                    onBackPressedDispatcher.onBackPressed()
+                }
+            }
+        })
+
+        if (savedInstanceState == null){
             supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, fragment)
-                .commit()
+                .replace(R.id.fragment_container, MainTaskListFragment()).commit()
+            navigationView.setCheckedItem(R.id.nav_home)
         }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.nav_home -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, MainTaskListFragment()).commit()
+
+            R.id.nav_about -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, AboutFragment()).commit()
+
+            R.id.nav_settings -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, SettingsFragment()).commit()
+
+            R.id.nav_deleted -> Toast.makeText(this, "previously deleted", Toast.LENGTH_SHORT)
+                .show()
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
