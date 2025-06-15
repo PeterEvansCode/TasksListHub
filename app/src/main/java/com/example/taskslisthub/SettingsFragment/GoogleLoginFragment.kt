99b254691e1b8335c9466a7efe5076a6370dfa54
Peter Evans <peterevans.code@gmail.com>
2025-06-15 14:01:35 +0100

    Debug and QoL

---
+++ app/src/main/java/com/example/taskslisthub/SettingsFragment/GoogleLoginFragment.kt
@@ -0,0 +1,150 @@
+package com.example.taskslisthub.SettingsFragment
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import com.bumptech.glide.Glide
+import com.example.taskslisthub.BuildConfig
+import com.example.taskslisthub.R
+import com.example.taskslisthub.TaskListFragment.TaskItemModelFactory
+import com.example.taskslisthub.TaskListFragment.TaskViewModel
+import com.example.taskslisthub.TasksListHub
+import com.example.taskslisthub.databinding.FragmentGoogleLoginBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.Scope
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import java.security.SecureRandom
+import java.util.Base64
+
+class GoogleLoginFragment : Fragment() {
+
+    //binding
+    private lateinit var binding: FragmentGoogleLoginBinding
+
+    // Assign viewModel
+    private val settingsViewModel: SettingsViewModel by viewModels {
+        SettingsViewModel.SettingsViewModelFactory((requireActivity().application as TasksListHub).repository)
+    }
+
+    //google tasks API
+    private lateinit var client: GoogleSignInClient
+    val REQUESTCODE = 10001
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        binding = FragmentGoogleLoginBinding.inflate(inflater, container, false)
+
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val options = GoogleSignInOptions.Builder()
+            .requestIdToken(BuildConfig.CLIENT_ID)
+            //.setAccountName(null.toString())
+            .requestScopes(Scope("https://www.googleapis.com/auth/tasks"))
+            .requestEmail()
+            .requestProfile()
+            .build()
+        client = GoogleSignIn.getClient(requireActivity(), options)
+
+        if (FirebaseAuth.getInstance().currentUser != null){
+            initialiseSignedIn()
+        }
+
+        else initialiseSignedOut()
+    }
+
+    private fun initialiseSignedIn(){
+        val signedInAccount = settingsViewModel.getGoogleAccount()
+
+        // Set up the Sign-In button
+        binding.signInButton.text = getString(R.string.signed_in_button)
+        binding.signInButton.setOnClickListener {
+            signOut()
+        }
+        //get account name
+        val accountName = signedInAccount?.email
+        binding.googleAccountName.text = accountName
+
+        //load profile picture
+        val photoUrl = signedInAccount?.photoUrl
+        Glide.with(this)
+            .load(photoUrl)
+            .into(binding.profileImage)
+    }
+
+    private fun initialiseSignedOut(){
+        // Set up the Sign-In button
+        binding.signInButton.text = getString(R.string.signed_out_button)
+        binding.signInButton.setOnClickListener {
+            signIn()
+        }
+
+        //set logged out text
+        binding.googleAccountName.text = getString(R.string.signed_out_account)
+
+        //set blank profile picture
+        binding.profileImage.setImageResource(R.drawable.logout_24)
+    }
+
+    private fun signIn() {
+        val intent = client.signInIntent
+        startActivityForResult(intent, REQUESTCODE)
+    }
+
+    private fun generateNonce(): String {
+        val random = SecureRandom()
+        val nonceBytes = ByteArray(16)
+        random.nextBytes(nonceBytes)
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(nonceBytes)
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUESTCODE) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+
+            try {
+                val account = task.getResult(ApiException::class.java) //error here
+                val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+                FirebaseAuth.getInstance().signInWithCredential(credential)
+                    .addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            settingsViewModel.setGoogleAccount(requireContext())
+                            initialiseSignedIn()
+                        } else {
+                            // Show a toast with the error message
+                            Toast.makeText(requireContext(), it.exception?.message, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            } catch (e: ApiException) {
+                // Handle the exception
+                Toast.makeText(requireContext(), "Sign-in failed: ${e.statusCode}", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
+    private fun signOut(){
+        FirebaseAuth.getInstance().signOut() //firebase sign-out
+        client.signOut().addOnCompleteListener {
+            initialiseSignedOut()  // Update UI after complete sign-out
+        }
+    }
+}
---
+++ app/src/main/java/com/example/taskslisthub/SettingsFragment/PersonaliseFragment.kt
@@ -0,0 +1,119 @@
+package com.example.taskslisthub.SettingsFragment
+
+import android.app.AlertDialog
+import android.content.Context
+import android.content.SharedPreferences
+import android.graphics.Color
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.Spinner
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.preference.PreferenceManager
+import com.example.taskslisthub.R
+import com.example.taskslisthub.databinding.FragmentPersonaliseBinding
+import com.github.dhaval2404.colorpicker.ColorPickerDialog
+
+class PersonaliseFragment : Fragment() {
+
+    private lateinit var binding: FragmentPersonaliseBinding
+
+    private lateinit var themeSpinner: Spinner
+    private lateinit var sharedPreferences: SharedPreferences
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = FragmentPersonaliseBinding.inflate(inflater, container, false)
+
+        // Initialize SharedPreferences
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(requireContext())
+
+        initialiseSpinner()
+        /*binding.changeColorButton.setOnClickListener{
+            showColorPicker()
+        }*/
+
+        return binding.root
+    }
+
+    private fun initialiseSpinner(){
+
+
+        // Initialize the spinner
+        themeSpinner = binding.themeSpinner
+
+        // Set the current selected item based on saved preference
+        val currentTheme = sharedPreferences.getString("theme_preference", "system")
+        val position = when (currentTheme) {
+            "light" -> 1
+            "dark" -> 2
+            else -> 0
+        }
+        themeSpinner.setSelection(position)
+
+        // Set listener for spinner selection
+        themeSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                val selectedTheme = when (position) {
+                    1 -> "light"
+                    2 -> "dark"
+                    else -> "system"
+                }
+                saveThemePreference(selectedTheme)
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // Do nothing
+            }
+        }
+    }
+
+    // Function to save the selected theme to SharedPreferences
+    private fun saveThemePreference(themeValue: String) {
+
+
+        // Get the theme preference value
+        val themePreference = sharedPreferences.getString("theme_preference", "system")
+
+        if (themePreference != themeValue) {
+            with(sharedPreferences.edit()) {
+                putString("theme_preference", themeValue)
+                apply()
+            }
+
+            // Apply the selected theme
+            applyChanges()
+        }
+    }
+
+    // Function to apply the selected theme
+    private fun applyChanges() {
+        requireActivity().recreate()
+    }
+
+    private fun showColorPicker() {
+        ColorPickerDialog
+            .Builder(requireContext()) // or `this` if using an Activity
+            .setTitle("Pick an Accent Color")
+            .setDefaultColor(R.color.colorAccent)
+            .setColorListener { color, colorHex ->
+                saveColorPreference(color)
+            }
+            .show()
+    }
+
+    private fun saveColorPreference(color: Int) {
+        val sharedPreferences = requireContext().getSharedPreferences("app_preferences", Context.MODE_PRIVATE)
+        with(sharedPreferences.edit()) {
+            putInt("colorPrimary", color)
+            apply()
+        }
+
+        applyChanges()
+    }
+}
